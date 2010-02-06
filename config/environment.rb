d39abe871930a14e8b39c7d7829128fdb55caa08
Reg Braithwaite <raganwald@gmail.com>
2010-02-06 01:44:27 -0500

    correct path to lib

--- config/environment.rb
+++ config/environment.rb
@@ -17,7 +17,7 @@ Rails::Initializer.run do |config|
   config.load_paths += %W( #{RAILS_ROOT}/rewritten/app/controllers )
   config.load_paths += %W( #{RAILS_ROOT}/rewritten/app/helpers )
   config.load_paths += %W( #{RAILS_ROOT}/rewritten/app/models )
-  config.load_paths += %W( #{RAILS_ROOT}/rewritten/app/lib )
+  config.load_paths += %W( #{RAILS_ROOT}/rewritten/lib )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem "haml"
