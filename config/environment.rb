e21c94aefb3d4b580e5bbf47924e99e901f89218
Reg Braithwaite <raganwald@gmail.com>
2010-02-06 01:12:13 -0500

    haml back in as a plugin

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   config.load_paths += %W( #{RAILS_ROOT}/rewritten/app/lib )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
+  config.gem "haml"
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
